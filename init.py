34fd2197507a31730b0904a020a3bb1e97c736a7
Livy M <olivia-meyer-02@outlook.com>
2023-08-05 13:03:43 +0100

    Audio added

--- init.py
+++ init.py
@@ -1,16 +1,18 @@
-#from playsound import playsound
+from playsound import playsound
 import threading 
 
+#Thread For Playing audio
 def audio ():
     print("Playing audio")
+    playsound('./Audio/Song1.mp3')
 
+#Thread Handles Leds
 def led ():
     print("Leds Activated")
 
-# Create two threads
+
 threadAudio = threading.Thread(target=audio, args=())
 threadLED = threading.Thread(target=led, args=())
-
 
 threadAudio.start()
 threadLED.start()
